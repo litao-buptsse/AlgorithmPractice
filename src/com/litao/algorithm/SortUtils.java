f23f4a1f5af70b9affa84459b05b49bae258a68f
litao <litao@sogou-inc.com>
2014-04-19 02:40:06 +0800

    Add mergeSort

--- src/com/litao/algorithm/SortUtils.java
+++ src/com/litao/algorithm/SortUtils.java
@@ -49,6 +49,61 @@ public class SortUtils {
 		}
 	}
 
+	public static <T> void mergeSort(T[] array) {
+		mergeSort(array, null);
+	}
+
+	public static <T> void mergeSort(T[] array, Comparator<T> comparator) {
+		if (array == null) {
+			return;
+		}
+
+		Comparator c;
+		if (comparator == null) {
+			c = InternalComparator.INSTANCE;
+		} else {
+			c = comparator;
+		}
+
+		T[] tmpArray = array.clone();
+		mergeSort(tmpArray, array, 0, array.length - 1, c);
+	}
+
+	private static <T> void mergeSort(T[] tmpArray, T[] array, int left, int right, Comparator<T> comparator) {
+		if (left < right) {
+			int middle = (left + right) / 2;
+			mergeSort(tmpArray, array, left, middle, comparator);
+			mergeSort(tmpArray, array, middle + 1, right, comparator);
+			merge(tmpArray, array, left, middle, right, comparator);
+		}
+	}
+
+	private static <T> void merge(T[] tmpArray, T[] array, int left, int middle, int right, Comparator<T> comparator) {
+		int i = left;
+		int j = middle + 1;
+		int k = left;
+
+		while (i <= middle && j <= right) {
+			if (comparator.compare(array[i], array[j]) <= 0) {
+				tmpArray[k++] = array[i++];
+			} else {
+				tmpArray[k++] = array[j++];
+			}
+		}
+
+		while (i <= middle) {
+			tmpArray[k++] = array[i++];
+		}
+
+		while (j <= right) {
+			tmpArray[k++] = array[j++];
+		}
+
+		for (int m = left; m <= right; m++) {
+			array[m] = tmpArray[m];
+		}
+	}
+
 	static class User implements Comparable<User> {
 		private int age;
 
@@ -77,13 +132,13 @@ public class SortUtils {
 					"age=" + age +
 					'}';
 		}
-
 	}
 
 	public static void main(String[] args) {
 		User[] users = {new User(4), new User(1), new User(5), new User(3), new User(2)};
 		// Arrays.sort(users);
-		SortUtils.bubbleSort(users);
+		// SortUtils.bubbleSort(users);
+		SortUtils.mergeSort(users);
 		for (User user : users) {
 			System.out.println(user);
 		}
