88b020acb1cc54bad3e402519ce27ac7e3054b08
Tao Li <litao@sogou-inc.com>
2014-09-22 15:49:18 +0800

    QuickSort and MergeSort support parallel compute by using jdk7 ForkJoin framework.

--- src/com/litao/algorithm/SortUtils.java
+++ src/com/litao/algorithm/SortUtils.java
@@ -1,200 +1,272 @@
 package com.litao.algorithm;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 
 /**
  * Created by Tao Li on 4/19/14.
  */
 public class SortUtils {
-	static class InternalComparator implements Comparator {
-		public static InternalComparator INSTANCE = new InternalComparator();
+    static class InternalComparator implements Comparator {
+        public static InternalComparator INSTANCE = new InternalComparator();
 
-		@Override
-		public int compare(Object o1, Object o2) {
-			return ((Comparable) o1).compareTo((Comparable) o2);
-		}
-	}
+        @Override
+        public int compare(Object o1, Object o2) {
+            return ((Comparable) o1).compareTo((Comparable) o2);
+        }
+    }
 
-	private static <T> void swap(T[] array, int i, int j) {
-		T tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
+    private static <T> void swap(T[] array, int i, int j) {
+        T tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
+    }
 
-	public static <T> void bubbleSort(T[] array) {
-		bubbleSort(array, null);
-	}
+    public static <T> void bubbleSort(T[] array) {
+        bubbleSort(array, null);
+    }
 
-	public static <T> void bubbleSort(T[] array, Comparator<T> comparator) {
-		if (array == null) {
-			return;
-		}
+    public static <T> void bubbleSort(T[] array, Comparator<T> comparator) {
+        if (array == null) {
+            return;
+        }
 
-		Comparator c;
-		if (comparator == null) {
-			c = InternalComparator.INSTANCE;
-		} else {
-			c = comparator;
-		}
+        Comparator c;
+        if (comparator == null) {
+            c = InternalComparator.INSTANCE;
+        } else {
+            c = comparator;
+        }
 
-		for (int i = array.length - 1; i >= 0; i--) {
-			for (int j = 0; j + 1 <= i; j++) {
-				if (c.compare(array[j], array[j + 1]) > 0) {
-					swap(array, j, j + 1);
-				}
-			}
-		}
-	}
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j + 1 <= i; j++) {
+                if (c.compare(array[j], array[j + 1]) > 0) {
+                    swap(array, j, j + 1);
+                }
+            }
+        }
+    }
 
-	public static <T> void mergeSort(T[] array) {
-		mergeSort(array, null);
-	}
+    static class MergeSortTask<T> extends RecursiveAction {
+        private T[] tmpArray;
+        private T[] array;
+        private int left;
+        private int right;
+        private Comparator<T> comparator;
 
-	public static <T> void mergeSort(T[] array, Comparator<T> comparator) {
-		if (array == null) {
-			return;
-		}
+        public MergeSortTask(T[] tmpArray, T[] array, int left, int right, Comparator<T> comparator) {
+            this.tmpArray = tmpArray;
+            this.array = array;
+            this.left = left;
+            this.right = right;
+            this.comparator = comparator;
+        }
 
-		Comparator c;
-		if (comparator == null) {
-			c = InternalComparator.INSTANCE;
-		} else {
-			c = comparator;
-		}
+        @Override
+        protected void compute() {
+            if (left < right) {
+                int middle = (left + right) / 2;
+                MergeSortTask<T> leftTask = new MergeSortTask<T>(tmpArray, array, left, middle, comparator);
+                leftTask.fork();
+                MergeSortTask<T> rightTask = new MergeSortTask<T>(tmpArray, array, middle + 1, right, comparator);
+                rightTask.fork();
 
-		T[] tmpArray = array.clone();
-		mergeSort(tmpArray, array, 0, array.length - 1, c);
-	}
+                leftTask.join();
+                rightTask.join();
+                merge(tmpArray, array, left, middle, right, comparator);
+            }
+        }
+    }
 
-	private static <T> void mergeSort(T[] tmpArray, T[] array, int left, int right, Comparator<T> comparator) {
-		if (left < right) {
-			int middle = (left + right) / 2;
-			mergeSort(tmpArray, array, left, middle, comparator);
-			mergeSort(tmpArray, array, middle + 1, right, comparator);
-			merge(tmpArray, array, left, middle, right, comparator);
-		}
-	}
+    public static <T> void mergeSort(T[] array, boolean parallel) {
+        mergeSort(array, null, parallel);
+    }
 
-	private static <T> void merge(T[] tmpArray, T[] array, int left, int middle, int right, Comparator<T> comparator) {
-		int i = left;
-		int j = middle + 1;
-		int k = left;
+    public static <T> void mergeSort(T[] array, Comparator<T> comparator, boolean parallel) {
+        if (array == null) {
+            return;
+        }
 
-		while (i <= middle && j <= right) {
-			if (comparator.compare(array[i], array[j]) <= 0) {
-				tmpArray[k++] = array[i++];
-			} else {
-				tmpArray[k++] = array[j++];
-			}
-		}
+        Comparator c;
+        if (comparator == null) {
+            c = InternalComparator.INSTANCE;
+        } else {
+            c = comparator;
+        }
 
-		while (i <= middle) {
-			tmpArray[k++] = array[i++];
-		}
+        T[] tmpArray = array.clone();
+        if (parallel) {
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+            MergeSortTask<T> task = new MergeSortTask<T>(tmpArray, array, 0, array.length - 1, c);
+            forkJoinPool.invoke(task);
+        } else {
+            mergeSort(tmpArray, array, 0, array.length - 1, c);
+        }
+    }
 
-		while (j <= right) {
-			tmpArray[k++] = array[j++];
-		}
+    private static <T> void mergeSort(T[] tmpArray, T[] array, int left, int right, Comparator<T> comparator) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(tmpArray, array, left, middle, comparator);
+            mergeSort(tmpArray, array, middle + 1, right, comparator);
+            merge(tmpArray, array, left, middle, right, comparator);
+        }
+    }
 
-		for (int m = left; m <= right; m++) {
-			array[m] = tmpArray[m];
-		}
-	}
+    private static <T> void merge(T[] tmpArray, T[] array, int left, int middle, int right, Comparator<T> comparator) {
+        int i = left;
+        int j = middle + 1;
+        int k = left;
 
-	public static <T> void quickSort(T[] array) {
-		quickSort(array, null);
-	}
+        while (i <= middle && j <= right) {
+            if (comparator.compare(array[i], array[j]) <= 0) {
+                tmpArray[k++] = array[i++];
+            } else {
+                tmpArray[k++] = array[j++];
+            }
+        }
 
-	public static <T> void quickSort(T[] array, Comparator<T> comparator) {
-		if (array == null) {
-			return;
-		}
+        while (i <= middle) {
+            tmpArray[k++] = array[i++];
+        }
 
-		Comparator c;
-		if (comparator == null) {
-			c = InternalComparator.INSTANCE;
-		} else {
-			c = comparator;
-		}
+        while (j <= right) {
+            tmpArray[k++] = array[j++];
+        }
 
-		quickSort(array, 0, array.length - 1, c);
-	}
+        for (int m = left; m <= right; m++) {
+            array[m] = tmpArray[m];
+        }
+    }
 
-	private static <T> void quickSort(T[] array, int left, int right, Comparator<T> comparator) {
-		if (left < right) {
-			int pivot = partitions(array, left, right, comparator);
-			quickSort(array, left, pivot - 1, comparator);
-			quickSort(array, pivot + 1, right, comparator);
-		}
-	}
+    static class QuickSortTask<T> extends RecursiveAction {
+        private T[] array;
+        private int left;
+        private int right;
+        private Comparator<T> comparator;
 
-	private static <T> int partitions(T[] array, int left, int right, Comparator<T> comparator) {
-		int i = left;
-		int j = right;
-		T tmp = array[i];
+        public QuickSortTask(T[] array, int left, int right, Comparator<T> comparator) {
+            this.array = array;
+            this.left = left;
+            this.right = right;
+            this.comparator = comparator;
+        }
 
-		while (i < j) {
-			while (i < j && comparator.compare(array[j], tmp) >= 0) {
-				j--;
-			}
-			if (i < j) {
-				array[i] = array[j];
-				i++;
-			}
-			while (i < j && comparator.compare(array[i], tmp) < 0) {
-				i++;
-			}
-			if (i < j) {
-				array[j] = array[i];
-				j--;
-			}
-		}
-		array[i] = tmp;
+        @Override
+        protected void compute() {
+            if (left < right) {
+                int pivot = partitions(array, left, right, comparator);
+                QuickSortTask<T> leftTask = new QuickSortTask<T>(array, left, pivot - 1, comparator);
+                leftTask.fork();
+                QuickSortTask<T> rightTask = new QuickSortTask<T>(array, pivot + 1, right, comparator);
+                rightTask.fork();
 
-		return i;
-	}
+                leftTask.join();
+                rightTask.join();
+            }
+        }
+    }
 
-	static class User implements Comparable<User> {
-		private int age;
+    public static <T> void quickSort(T[] array, boolean parallel) {
+        quickSort(array, null, parallel);
+    }
 
-		public User(int age) {
-			this.age = age;
-		}
+    public static <T> void quickSort(T[] array, Comparator<T> comparator, boolean parallel) {
+        if (array == null) {
+            return;
+        }
 
-		@Override
-		public int compareTo(User user) {
-			if (user == null) {
-				throw new NullPointerException("compareTo param is null");
-			}
+        Comparator c;
+        if (comparator == null) {
+            c = InternalComparator.INSTANCE;
+        } else {
+            c = comparator;
+        }
 
-			if (this.age > user.age) {
-				return 1;
-			} else if (this.age == user.age) {
-				return 0;
-			} else {
-				return -1;
-			}
-		}
+        if (parallel) {
+            ForkJoinPool forkJoinPool = new ForkJoinPool();
+            QuickSortTask<T> task = new QuickSortTask<T>(array, 0, array.length - 1, c);
+            forkJoinPool.invoke(task);
+        } else {
+            quickSort(array, 0, array.length - 1, c);
+        }
+    }
 
-		@Override
-		public String toString() {
-			return "User{" +
-					"age=" + age +
-					'}';
-		}
-	}
+    private static <T> void quickSort(T[] array, int left, int right, Comparator<T> comparator) {
+        if (left < right) {
+            int pivot = partitions(array, left, right, comparator);
+            quickSort(array, left, pivot - 1, comparator);
+            quickSort(array, pivot + 1, right, comparator);
+        }
+    }
 
-	public static void main(String[] args) {
-		User[] users = {new User(4), new User(1), new User(5), new User(3), new User(2)};
-		// Arrays.sort(users);
-		// SortUtils.bubbleSort(users);
-		// SortUtils.mergeSort(users);
-		SortUtils.quickSort(users);
-		for (User user : users) {
-			System.out.println(user);
-		}
-	}
+    private static <T> int partitions(T[] array, int left, int right, Comparator<T> comparator) {
+        int i = left;
+        int j = right;
+        T tmp = array[i];
+
+        while (i < j) {
+            while (i < j && comparator.compare(array[j], tmp) >= 0) {
+                j--;
+            }
+            if (i < j) {
+                array[i] = array[j];
+                i++;
+            }
+            while (i < j && comparator.compare(array[i], tmp) < 0) {
+                i++;
+            }
+            if (i < j) {
+                array[j] = array[i];
+                j--;
+            }
+        }
+        array[i] = tmp;
+
+        return i;
+    }
+
+    static class User implements Comparable<User> {
+        private int age;
+
+        public User(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public int compareTo(User user) {
+            if (user == null) {
+                throw new NullPointerException("compareTo param is null");
+            }
+
+            if (this.age > user.age) {
+                return 1;
+            } else if (this.age == user.age) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "age=" + age +
+                    '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        User[] users = {new User(4), new User(1), new User(5), new User(3), new User(2)};
+        // Arrays.sort(users);
+        // SortUtils.bubbleSort(users);
+        // SortUtils.mergeSort(users, false);
+        // SortUtils.mergeSort(users, true);
+        // SortUtils.quickSort(users, false);
+        SortUtils.quickSort(users, true);
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
 }
